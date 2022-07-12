43b0859e5e0f5d8be81b5c94cb80dcd6be9042d4
Vincent Le <levin@oregonstate.edu>
2022-07-12 10:39:52 -0700

    read command line arguments implemented

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,6 +3,13 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length ==0){
+            System.out.println("Please specify a name and email");
+            System.exit(1);
+        } else if (args.length == 1) {
+            System.out.println("Please specify an email for " + args[0]);
+            System.exit(1);
+        }
+        System.out.println(args[0] + " " + "<" + args[1] + ">");
     }
 }
